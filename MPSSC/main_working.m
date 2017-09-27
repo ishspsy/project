a35cc94af6751051a017472be470735f3e099d36
ishspsy <ishspsy@gmail.com>
2017-09-27 01:52:26 -0400

    Update main_working.m

--- MPSSC/main_working.m
+++ MPSSC/main_working.m
@@ -6,7 +6,8 @@ addpath('Data')
 load('deng_data.mat')
 X=in_X; Y=X;  [n,p]=size(X); N=n;
 
-rho=0.2; lam=0.0001; lam2=lam; eta=1; c=0.1; cases=1;  %cases=1 when sparse kernels are used.
+rho=0.2; lam=0.0001; lam2=lam; eta=1; c=0.1;  
+%%cases=1 when sparse kernels are used. use cases=2 for general case;  use cases=1 for ginhoux and deng_data cases only
 [P2,perf_fin] = clus_fin_update(rho, lam, lam2, eta, c, X, true_labs, cases);
 
 
