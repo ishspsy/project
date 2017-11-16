89b98924738843ddf22f22bcf94b62dcace34f9c
ishspsy <ishspsy@gmail.com>
2017-11-16 16:54:02 +0900

    Update running_large_scRNA (Fig5).m

--- MPSSC/Working_m_files/running_large_scRNA (Fig5).m
+++ MPSSC/Working_m_files/running_large_scRNA (Fig5).m
@@ -15,18 +15,34 @@ clear all
 load('Data_Tasic.mat')
 in_X=double(in_X);  % the data of Tasic is single-type (due to size issue), thus need to be transferred to double type.
 
+% run the other 6 clustering methods: each row of 'valtot' stands for NMI, Purity, and ARI, respectively.
 real_data_place_large
 save('realdata10_tasic_except.mat','valtot')
+
+% run MPSSC and PSSC: 
+% tot_mpssc and toc_mppsc are three performance measures and computation time (seconds) of MPSSC, respectively,
+% tot_mpssc2 and toc_mppsc2 are three performance measures and computation time (seconds) of PSSC, respectively,
 Larger_MPSSC
 save('Tasic_MPSSC.mat', 'tot_mpssc','tot_mpssc2','toc_mpssc','toc_mpssc2')
+
+
+% run MPSSC with various \lambda values from the set [0.00001, 0.00005, 0.0001, 0.0002, 0.0005, 0.001, 0.01, 0.1] for
+% robustness check
 robust_generator_large
 save('Tasic_robust_lam.mat', 'tot_mpssc_set')
+
+% run MPSSC with various \rho values from the set [0.001,0.01, 0.1, 0.2, 0.5, 1, 2, 5,10]for robustness check.
 robust_generator_large_rhos
 save('Tasic_robust_rho.mat', 'tot_mpssc_set')
+
+% run MPSSC with various c values from the set [0.0001, 0.001,0.01, 0.1, 0.2, 0.5, 1] for robustness check.
 robust_generator_large_cs
 save('Tasic_robust_c.mat', 'tot_mpssc_set')
+
+% run MPSSC with various k values from the set [5, 10, ..., 35, 40] for robustness check.
 robust_generator_large_k
 save('Tasic_robust_k.mat', 'tot_mpssc_set')
+
 
 
 %% Zeisel
