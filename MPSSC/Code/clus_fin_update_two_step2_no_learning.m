f4ac4b3b239b35256b28c1f0b87478be0a589c16
ishspsy <ishspsy@gmail.com>
2017-11-16 16:18:19 +0900

    Update clus_fin_update_two_step2_no_learning.m

--- MPSSC/Code/clus_fin_update_two_step2_no_learning.m
+++ MPSSC/Code/clus_fin_update_two_step2_no_learning.m
@@ -1,7 +1,7 @@
 function [P2,P3] = clus_fin_update_two_step2_no_learning(rho, lam, lam2, eta, c, X, true_labs,k10)
 %% running PSSC for large-scale scRNA-seq data sets without learning similarity matrix
 
-[Kernels, id]= func_doubly_1022(X, k10); [n,p]=size(X);   
+[Kernels, id]= func_doubly(X, k10); [n,p]=size(X);   
 CCC=max(true_labs);
 [rep, W, P2, Q,P3]=clus_sim_update2_2_two_no_learning(CCC, c, rho, lam, id, eta, single(Kernels)) ;
 end
